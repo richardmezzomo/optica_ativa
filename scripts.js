769563841e0936b86d29f893dfaba860ae7ed527
Richard Mezzomo <richardmezzomo1@gmail.com>
2023-09-05 19:51:31 -0300

    fix: some bugs

--- scripts.js
+++ scripts.js
@@ -1,5 +1,38 @@
 const openMenuButton = document.querySelector('.open-menu');
 const menuNavigation = document.querySelector('.menu-navigation');
+const navigation = document.querySelector('.navigation-mobile');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    navigation.classList.add('navigation--scroll');
+  } else {
+    navigation.classList.remove('navigation--scroll');
+  }
+});
+
+
+let lastScrollTop = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+  const scrollTop = window.scrollY;
+
+  // Verifica a direção do scroll
+  if (scrollTop > lastScrollTop) {
+    // Rolagem para baixo
+    header.classList.add('header-hidden');
+    navigation.classList.add('navigation-mobile-hidden');
+  } else {
+    // Rolagem para cima
+    header.classList.remove('header-hidden');
+    navigation.classList.remove('navigation-mobile-hidden');
+  }
+
+  lastScrollTop = scrollTop;
+});
+
+
+
 
 openMenuButton.addEventListener('click', () => {
   openMenuButton.classList.toggle('open');
